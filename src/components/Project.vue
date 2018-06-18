39b3ba563c01acacf60af62d40e6cf427a9ca98f
Catalin Zalog <c@zalog.ro>
2018-06-19 00:25:03 +0300

    fix: equalize projects `v-card` heights

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,21 +1,17 @@
 <template>
-  <div>
-
-    <v-card color="blue-grey lighten-5 project" class="black--text" height="100%" flat>
-      <v-card-title primary-title>
-        <div>
-          <h3 class="headline">{{ name }}</h3>
-          <div v-html="getSkills"></div>
-        </div>
-      </v-card-title>
-      <v-card-actions>
-        <v-btn :href="href" target="_blank" flat>Visit</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn flat disabled>{{ year }}</v-btn>
-      </v-card-actions>
-    </v-card>
-
-  </div>
+  <v-card color="blue-grey lighten-5 project" class="black--text" height="100%" flat>
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline">{{ name }}</h3>
+        <div v-html="getSkills"></div>
+      </div>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn :href="href" target="_blank" flat>Visit</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn flat disabled>{{ year }}</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
