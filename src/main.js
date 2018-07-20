d92cb8b6449795fe120b5fd5a1aee0d77dc6163a
Catalin Zalog <c@zalog.ro>
2018-07-20 10:50:48 +0300

    feat: adds google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -7,6 +8,10 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-1205565-7'
+})
 
 Vue.use(Vuetify)
 
